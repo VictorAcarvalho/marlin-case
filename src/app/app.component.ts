9f334c996b7f2b01eee72f12bf3d41b9862bfcff
VictorAcarvalho <carvalho.victorfa@gmail.com>
2022-02-13 15:30:37 -0300

    feat: add head-components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: ` 
-    <h1>Hello {{title}}</h1>
-  
-  
-  `
-    
+  templateUrl:'app.component.html'
 })
 export class AppComponent {
-  title = 'World!';
+  
 }
