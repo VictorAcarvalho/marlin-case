7de9085685f5f325c6a5a6e939c055d2aa2ad368
VictorAcarvalho <carvalho.victorfa@gmail.com>
2022-02-13 18:35:33 -0300

    feat:add newscrud view and routing project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { NewslatterComponent} from './views/newslatter/newslatter.component';
+import { NewsCrudComponent} from './views/news-crud/news-crud.component';
+const routes: Routes = [{
+  path:"",
+  component:NewslatterComponent
+},{
+  path:"newscreate",
+  component:NewsCrudComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
