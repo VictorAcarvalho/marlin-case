a89834f5d1a32b5f32f7b6a1075a8d0397f87f16
VictorAcarvalho <carvalho.victorfa@gmail.com>
2022-02-14 12:30:08 -0300

    feat:add method createNews in service

--- src/app/views/news-crud/news-crud.component.ts
+++ src/app/views/news-crud/news-crud.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NewsService } from '../../services/news.service';
+import { INews } from '../../utils/news.model';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -7,11 +10,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./news-crud.component.css']
 })
 export class NewsCrudComponent implements OnInit {
+  news:INews ={
+    title:'',
+    image:'',
+    body:''
+  }
 
-  constructor( ) { }
+  constructor( private newsService: NewsService, private router:Router) { }
 
 
   ngOnInit(): void {
   }
   
+  createNewsletter():void{
+    this.newsService.createNews(this.news).subscribe(()=>{ 
+        this.router.navigate(['/'])
+      }
+    )
+  }
 }
